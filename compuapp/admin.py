728c688778d900db5707f4e242c1e99e8e6b598d
Simon Cubilla <bernardo.cubilla@mec.edu.py>
2023-07-04 15:07:05 -0400

    14 datetime

--- compuapp/admin.py
+++ compuapp/admin.py
@@ -5,7 +5,7 @@ from .models import Computer
 from .forms import ComputerForm
 
 class ComputerAdmin(admin.ModelAdmin):
-    list_display = ["computer_name", "IP_address", "MAC_address"]
+    list_display = ["computer_name", "IP_address", "users_name", "MAC_address", "purchase_date", "timestamp"]
     form = ComputerForm
     list_filter = ['computer_name', 'IP_address']
     search_fields = ['computer_name', 'IP_address']
--- compuapp/models.py
+++ compuapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Computer(models.Model):
@@ -7,7 +8,8 @@ class Computer(models.Model):
     MAC_address = models.CharField(max_length=30)
     users_name = models.CharField(max_length=30)
     location = models.CharField(max_length=30)
-
+    purchase_date = models.DateField(auto_now_add=False, auto_now=False, blank=True, null=True)
+    timestamp = models.DateField(auto_now_add=True, auto_now=False, blank=True)
     def __str__(self):
         return self.IP_address + ' ' + self.computer_name
 
